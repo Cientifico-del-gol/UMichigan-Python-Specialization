24a87a56a48c31b06b5f1b16702f77bc07cb9c73
Cientifico-del-gol <162631539+Cientifico-del-gol@users.noreply.github.com>
2024-03-12 14:37:35 -0500

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,6 @@
+hours=input('Hourerinos: ')
+rate=input('Raterino: ')
+hrs=float(hours)
+rt=float(rate)
+pay=hrs*rt
+print('Payerino:',pay)
---
+++ ex4.py
@@ -0,0 +1,5 @@
+#Celsius to fahrenheit conversor
+temperature=input('Enterino celsius temperature: ')
+temp=float(temperature)
+temp2=(temp*1.8)+32
+print(temp2)
